58739d38c4ca40cd7001d663b3b2d28c159154d9
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2015-10-14 15:53:25 -0400

    Adding files

---
+++ src/internal/trycatch.js
@@ -0,0 +1,21 @@
+var errorObj = {e: {}};
+
+function tryCatcherGen(tryCatchTarget) {
+  return function tryCatcher() {
+    try {
+      return tryCatchTarget.apply(this, arguments);
+    } catch (e) {
+      errorObj.e = e;
+      return errorObj;
+    }
+  };
+}
+
+function tryCatch(fn) {
+  if (!isFunction(fn)) { throw new TypeError('fn must be a function'); }
+  return tryCatcherGen(fn);
+}
+
+function thrower(e) {
+  throw e;
+}
