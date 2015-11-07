30647a5cbd163a88982ed94bc4a7ea4d52b0ab3b
paul.giletich <paul.giletich@gmail.com>
2015-11-07 04:08:27 +0300

    fix safeApply throwing "cannot read property ''$$phase' of null" on destroyed scopes

--- src/safeApply.js
+++ src/safeApply.js
@@ -3,9 +3,13 @@
   Rx.Observable.prototype.safeApply = function($scope, fn){
     fn = angular.isFunction(fn) ? fn : noop;
 
-    return this.tap(function (data) {
-      ($scope.$$phase || $scope.$root.$$phase) ?
-        fn(data) :
-        $scope.$apply(function () { fn(data); });
+    return this
+      .takeWhile(function () {
+        return !$scope.$$destroyed;
+      })
+      .tap(function (data) {
+        ($scope.$$phase || $scope.$root.$$phase) ?
+          fn(data) :
+          $scope.$apply(function () { fn(data); });
     });
   };
