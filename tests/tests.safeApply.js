a3e162e64802ca493bfd8513a832d5514fb69e3b
Giles Roadnight <giles@roadnight.name>
2016-01-26 10:42:36 +0000

    added onError and onComplete parameters to safeApply updated tests

--- tests/tests.safeApply.js
+++ tests/tests.safeApply.js
@@ -1,28 +1,101 @@
 module('safeApply');
 
-asyncTest('calls $apply', function () {
+asyncTest('calls $apply onNext', function () {
   var injector = angular.injector(['ng', 'rx']);
 
   var scope = injector.get('$rootScope').$new();
 
-  scope.$watch('testProperty', function (val) {
-    scope.testProperty2 = val;
-  });
+  var source = new Rx.Subject();
 
-  scope
-    .$toObservable('testProperty')
+  var result;
+
+  source
     .debounce(500)
-    .safeApply(scope, function (val) {
-      scope.testProperty = 2;
-    })
+    .safeApply(scope,
+      function (val) {
+        result = val;
+      },
+      function (error) {
+        result = error;
+      }
+    )
     .subscribe(function () {
       start();
-      ok(scope.testProperty2 === 2);
+      ok(result === 2);
     });
 
-  scope.$apply(function () {
-    scope.testProperty = 1;
-  });
+  source.onNext( 2 );
+
+  expect(1);
+});
+
+asyncTest('calls $apply onError', function () {
+  var injector = angular.injector(['ng', 'rx']);
+
+  var scope = injector.get('$rootScope').$new();
+
+  var source = new Rx.Subject();
+
+  var result;
+
+  var expectedError = {details: "expected error details"};
+
+  source
+    .debounce(500)
+    .safeApply(scope,
+      function (val) {
+        result = val;
+      },
+      function (error) {
+        result = error;
+      }
+    )
+    .subscribe(
+      function(){},
+      function() {
+      start();
+      ok(result === expectedError);
+    });
+
+  source.onError( expectedError );
+
+  expect(1);
+});
+
+asyncTest('calls $apply onComplete', function () {
+  var injector = angular.injector(['ng', 'rx']);
+
+  var scope = injector.get('$rootScope').$new();
+
+  var source = new Rx.Subject();
+
+  var result;
+
+  var expectedError = {};
+
+  source
+    .debounce(500)
+    .safeApply(scope,
+      function (val) {
+        result = val;
+      },
+      function (error) {
+        result = error;
+      },
+      function () {
+        result = 4;
+      }
+    )
+    .subscribe(
+      function(){},
+      function(){},
+      function () {
+      ok(result === 4);
+    });
+
+  start();
+  source.onNext( 2 );
+  source.onCompleted();
 
   expect(1);
 });
